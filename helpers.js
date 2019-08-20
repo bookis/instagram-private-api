7a1078c77b519d00587b17620f9f83266ef235f2
ifedapoolarewaju <ifedapoolarewaju@gmail.com>
2019-08-20 18:40:21 +0100

    better message URL parsing

--- helpers.js
+++ helpers.js
@@ -118,7 +118,8 @@ Helpers.dataToRequestOption = function (data, filename) {
 }
 
 Helpers.extractUrl = function (text) {
-  return text.match(/((?:https\:\/\/)|(?:http\:\/\/)|(?:www\.))?([a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(?:\??)[a-zA-Z0-9\-\._\?\,\'\/\\\+&%\$#\=~]+)/g);
+    var URL_REGEX = new RegExp(/(http:\/\/|https:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/, 'i')
+    return text.match(URL_REGEX);
 }
 
 module.exports = Helpers;
